650a559a2f3d9700b6160c808ee48579f0f45d00
liyuhao <liyuhaoid@sina.com>
2017-06-05 16:33:49 +0800

    Initial commit

---
+++ app/src/main/java/spa/lyh/cn/fullpermisson/MainActivity.java
@@ -0,0 +1,45 @@
+package spa.lyh.cn.fullpermisson;
+
+import android.Manifest;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends PermissionActivity {
+    int my;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.btn1:
+                my = 1;
+                if (hasPermission(Manifest.permission.CAMERA,Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.READ_EXTERNAL_STORAGE)){
+                    doAfterPermission();
+                }
+                break;
+            case R.id.btn2:
+                my = 2;
+                if (hasPermission(Manifest.permission.CAMERA)){
+                    doAfterPermission();
+                }
+                break;
+        }
+    }
+
+
+    @Override
+    public void doAfterPermission() {
+        switch (my){
+            case 1:
+                Toast.makeText(this,"方法1",Toast.LENGTH_SHORT).show();
+                break;
+            case 2:
+                Toast.makeText(this,"方法2",Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+}
